cbac9a19e481ae7c724cbe0892b3b927f75c3d28
Pranjal Patil <pranjalpatil0705@gmail.com>
2026-04-08 20:32:14 +0530

    Initial commit - cloud project

---
+++ backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fullstackamplifynote": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "fullstackamplifynote44ac6410": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "imagestore": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
